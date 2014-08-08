f827502c316e18d82acdf90622226cf9c3f79786
javamaster <mauricio.ads10@gmail.com>
2014-08-08 15:52:57 -0300

    Ok

---
+++ Main2.java
@@ -0,0 +1,22 @@
+public class Main{
+
+public static void main(String[] args){
+
+	int num = Integer.parseInt(args[1]);
+	
+	int a=0, count = 0;
+	int b = 1;
+	
+	while(count<num){
+		
+		System.out.println(a);
+		System.out.println(b);
+		a = b;
+		b = a + b;
+
+		
+	}
+
+}
+
+}
